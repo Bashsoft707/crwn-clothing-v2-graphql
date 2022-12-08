31bd844d5b47aad7f3c9c667e4df98420e3062f3
Bashsoft707 <alausabashir@gmail.com>
2022-12-08 08:49:44 -0800

    mobile responsiveness

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Spinner from "./components/spinner/spinner.component";
+import { GlobalStyle } from "./global.style";
 
 const Home = lazy(() => import("./routes/home/home.component"));
 const Navigation = lazy(() =>
@@ -15,6 +16,7 @@ const Checkout = lazy(() => import("./routes/checkout/checkout.component"));
 const App = () => {
   return (
     <Suspense fallback={<Spinner />}>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<Navigation />}>
           <Route index element={<Home />} />
